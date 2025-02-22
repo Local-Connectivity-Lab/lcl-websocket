d91774b9b3e46bc426ef69005b32f64e3bc7c943
Zhennan Zhou <zhouz46@uw.edu>
2025-02-22 00:04:24 -0800

    update swiftnio to 2.81.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.80.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.81.0"),
         .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.23.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.28.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.6.2"),
